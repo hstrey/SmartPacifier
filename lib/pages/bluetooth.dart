c512eff18698eb76d545c459943bccd3121b30f1
ashleyvalenton <ashley.valenton@stonybrook.edu>
2023-10-16 13:20:54 -0400

    added bluetooth UI

--- lib/pages/bluetooth.dart
+++ lib/pages/bluetooth.dart
@@ -1,17 +1,76 @@
 import 'package:flutter/material.dart';
 
-class Bluetooth extends StatelessWidget {
-  Bluetooth({Key? key, required this.title}) : super(key: key);
+class Bluetooth extends StatefulWidget {
+  const Bluetooth({super.key, required this.title});
 
   final String title;
+
+  @override
+  State<Bluetooth> createState() => _BluetoothState();
+}
+
+class _BluetoothState extends State<Bluetooth> {
+  String buttonName = 'Connect Device';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: Text(title)
-        )
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const Wrap(
+        runSpacing: 25.0,
+        alignment: WrapAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 50.0, left: 10.0, right: 10.0),
+            child: Text(
+              "These are the instructions on how to connect to your Bluetooth Smart Pacifier.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+            ),
+          ),
+          ConnectionButton(),
+          ConnectionButton(),
+          ConnectionButton(),
+          ConnectionButton(),
+          ConnectionButton(),
+        ],
+      ),
+    );
+  }
+}
+
+class ConnectionButton extends StatefulWidget {
+  const ConnectionButton({Key? key}) : super(key: key);
+
+  @override
+  State<ConnectionButton> createState() => _ConnectionButtonState();
+}
+
+class _ConnectionButtonState extends State<ConnectionButton> {
+  String buttonName = 'Connect Device';
+  bool buttonA = false;
+  bool buttonB = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          fixedSize: const Size(350, 50),
+          textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.deepPurple,
+          shape: const StadiumBorder()),
+      child: buttonB ? const Text("Connected") : const Text("Connect Device"),
+      onPressed: () {
+        setState(() {
+          buttonA = !buttonA;
+          buttonB = !buttonB;
+        });
+      },
     );
   }
 }
